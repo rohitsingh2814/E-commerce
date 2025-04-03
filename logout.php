ea87051cb56c93392cc0516a5184c86f086cb4a7
Rohit Singh <rohitsingh7667142299@gmail.com>
2025-04-03 23:29:11 +0530

    add new tailwind header but login work correctly

--- logout.php
+++ logout.php
@@ -1,8 +1,5 @@
 <?php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-
+session_start();
 session_unset();
 session_destroy();
 header("Location: index.php?home=true"); // Redirect to home after logout
