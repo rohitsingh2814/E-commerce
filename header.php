f7b1999ca8f0b1313ee09bb8568e9b11b2dace2b
Rohit Singh <rohitsingh7667142299@gmail.com>
2025-04-06 22:26:20 +0530

    header

--- header.php
+++ header.php
@@ -1,8 +1,7 @@
-
 <?php
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
-  }
+}
 
 if (isset($_GET['logout'])) {
   session_start();
@@ -11,11 +10,10 @@ if (isset($_GET['logout'])) {
   exit();
 } 
 ?>
-
 <header>
-
+  
 <!-- fixed top-0 left-0 w-full z-50 bg-white shadow-md -->
-  <nav class="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+  <nav class="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700 fixed top-0 left-0 w-full z-50 bg-white shadow-md">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <a href="?home=true" class="flex items-center space-x-3 rtl:space-x-reverse">
         <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"><i class="fa-solid fa-bag-shopping me-2"></i>TechShop</span>
@@ -55,7 +53,7 @@ if (isset($_GET['logout'])) {
                   <a href="?logout=true" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"><i class="fa-solid fa-user-minus me-2"></i> Sign out</a>
                 </div>
               </div>
-            </li><?php }else{?><li><a href="?login=true" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"><i class="fa-solid fa-user me-2"></i> Login</a></li><?php }?><li><a href="?wishlist=true" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"><i class="fa-regular fa-heart me-2"></i> Wishlist</a></li>
+            </li><?php } else{?><li><a href="?login=true" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"><i class="fa-solid fa-user me-2"></i> Login</a></li><?php }?><li><a href="?wishlist=true" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"><i class="fa-regular fa-heart me-2"></i> Wishlist</a></li>
           <li><a href="?cart=true" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"><i class="fa-solid fa-cart-shopping me-2"></i> Cart</a></li>
         </ul>
       </div>
