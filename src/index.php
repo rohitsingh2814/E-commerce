c0bcda33acbc6c15d9ab90a9a6eb0ca0480d5444
AlphaGaurav13 <gauravkkr345@gmail.com>
2025-04-06 21:42:16 +0530

    Shifted the Whole CDN Dependendcy To Production Based Format Ready Now Use TailWind using terminal

---
+++ src/index.php
@@ -0,0 +1,26 @@
+<?php include_once 'header.php';
+if(isset($_GET['login'])){
+    include ('login.php');
+}
+elseif(isset($_GET['signup'])){
+    include ('signup.php');
+}
+elseif(isset($_GET['logout'])){
+    include ('logout.php');
+}
+elseif(isset($_GET['home'])){
+    include ('home.php');
+}
+elseif(isset($_GET['wishlist'])){
+    include ('wishlist.php');
+}
+elseif(isset($_GET['cart'])){
+    include ('cart.php');
+}
+else{
+    include ('home.php'); 
+}
+?>
+
+
+<!-- dajhrhf -->
---
+++ src/partials/commonfiles.php
@@ -0,0 +1,2 @@
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
