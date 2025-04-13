df1a436af6efa50fb8869c87f162ab53ae3d0c2b
AlphaGaurav13 <gauravkkr345@gmail.com>
2025-04-13 12:34:23 +0530

    Now we can take Customer feedBack!

--- src/Help.php
+++ src/Help.php
@@ -4,6 +4,7 @@ if (session_status() === PHP_SESSION_NONE) {
   session_start();
 }
 
+
 // Check if user is not logged in
 if (!isset($_SESSION['username'])) {
   // Redirect to login
@@ -14,7 +15,35 @@ if (!isset($_SESSION['username'])) {
 ?>
 
 
-<?php include 'form.php' ?>
+<?php
+$showAlert = false;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
+
+  include 'partials/database.php';
+  $username = $_POST["name"];
+  $email = $_POST["email"];
+  $problem = $_POST["problem"];
+  $sql = "INSERT INTO `queryt` ( `name`, `email`, `content`) VALUES ('$username', '$email', '$problem')";
+  $result = mysqli_query($conn, $sql);
+
+  if ($result) {
+    $showAlert = true;
+  }
+}
+
+
+?>
+
+
+<?php if ($showAlert): ?>
+  <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4" role="alert">
+    <span class="block sm:inline">Your query has been registered, explore more products!</span>
+  </div>
+<?php endif; ?>
+
+<form method="POST">
+  <?php include 'form.php'; ?>
+</form>
 
 <?php include 'footer.php'; ?>
